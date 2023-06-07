b29fee5d5dc5e5b536bb5196f7f8b99fd9848ba1
Tom Rives <tom.rives@epitech.eu>
2023-06-07 15:12:51 +0900

    [FIX]

--- Assets/Scripts/ScoreManager.cs
+++ Assets/Scripts/ScoreManager.cs
@@ -6,11 +6,13 @@ using UnityEngine;
 public class ScoreManager : MonoBehaviour
 {
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI scoreGameOverText;
     public TextMeshProUGUI lifeText;
 
     public void UpdateScore(int score)
     {
         scoreText.text = score.ToString();
+        scoreGameOverText.text = score.ToString();
     }
 
     public void UpdateLife(int life)
